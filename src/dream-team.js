201650ae6a6c3d9c859d5b85295ed857a453325f
Yurchak Veronika <yurchakveronika1997@gmail.com>
2021-03-04 14:57:40 +0300

    feat: add function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arrayName) {
+  let teamName = [];
+  if(arrayName===null||arrayName===undefined){
+    return false;
+  }
+  for(let i=0;i<arrayName.length;i++){
+    if(typeof arrayName[i]==="string"){
+      arrayName[i]= arrayName[i].replace(/^ +/gm, '');
+      teamName.push(arrayName[i][0].toUpperCase());
+    }
+  }
+
+  teamName.sort();
+  if(teamName.length>0){
+    return teamName.join('');
+  }
+  return false;
 };
