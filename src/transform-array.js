57023c5ca54f83d0b0b9be632afa13174ac53b41
Yurchak Veronika <yurchakveronika1997@gmail.com>
2021-03-05 16:04:25 +0300

    feat: add function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+
+    if(Array.isArray(array)){
+        let result = [];
+        let i = 0;
+        const sequences = {
+            '--discard-next': function (){
+                i++;
+            },
+            '--discard-prev': function (){
+                if(result[result.length-1]===array[i-1]){
+                    result.pop();}
+            },
+            '--double-next': function (){
+                if(array[i+1]!==undefined){
+                    result.push(array[i+1])}
+            },
+            '--double-prev': function (){
+                if(array[i-1]!==undefined&&array[i-1]===result[result.length-1]){
+                    result.push(array[i-1])}
+            }
+        }
+        while(i<array.length){
+
+            if(sequences[array[i]]!==undefined){
+                sequences[array[i]]();}
+
+            else if(array[i]===result[result.length-1]){
+                continue;
+            }
+
+            else{
+                result.push(array[i]);
+            }
+            i++;
+        }
+
+        return result;
+    }
+    if(array.length===0){
+        return [];
+    }
+    else {
+        throw new Error('Error')
+    }
+
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if(date===null||date===undefined){
+        return 'Unable to determine the time of year!'
+    }
+    if(Object.prototype.toString.call(date) === '[object Date]') {
+        let Date = date;
+        let month = Date.getMonth();
+        if (month < 2 || month === 11) {
+            return 'winter'
+        }
+        if (month < 5) {
+            return 'spring';
+        }
+        if (month < 8) {
+            return 'summer'
+        }
+        if (month < 11) {
+            return 'autumn'
+        }
+    }
+    else {
+        throw new Error('Error')
+    }
 };
